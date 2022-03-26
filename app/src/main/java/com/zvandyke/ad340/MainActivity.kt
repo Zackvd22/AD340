062474daa478fdbc79edb28f8479f77b77e804f0
Zackvd22 <42554592+Zackvd22@users.noreply.github.com>
2022-03-25 22:44:25 -0400

    Changes after lecture 2. Added dimens.xml Added simple UI for input of a zip code and a toast to display the input or an error message.

--- app/src/main/java/com/zvandyke/ad340/MainActivity.kt
+++ app/src/main/java/com/zvandyke/ad340/MainActivity.kt
@@ -2,10 +2,32 @@ package com.zvandyke.ad340
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val zipcodeEditText: EditText = findViewById(R.id.zipcoceEditText)
+        val enterButton: Button = findViewById(R.id.enterButton)
+
+        enterButton.setOnClickListener()
+        {
+            val zipcode: String = zipcodeEditText.text.toString()
+
+            if (zipcode.length != 5)
+            {
+                Toast.makeText(this, R.string.invalidZipcodeLen, Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
